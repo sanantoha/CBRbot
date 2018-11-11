8589d42d47e1b08e86c1fe8e911f96afd41db357
san <sanantoha@gmail.com>
2018-11-11 17:06:08 +0300

    Add type to linebacker to make idea green

--- src/test/scala/com/bot/cbr/config/ConfigSpec.scala
+++ src/test/scala/com/bot/cbr/config/ConfigSpec.scala
@@ -18,9 +18,10 @@ class ConfigSpec extends UnitSpec {
   val testEc = ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor())
 
   "CBRConfig " should "read config" in {
-    val ioRes: IO[Config] = E.unbound[IO].map(Linebacker.fromExecutorService[IO]).use { implicit linebacker =>
-      implicit val _: ContextShift[IO] = IO.contextShift(testEc)
-      load[IO]
+    val ioRes: IO[Config] = E.unbound[IO].map(Linebacker.fromExecutorService[IO]).use {
+      implicit linebacker: Linebacker[IO] =>
+        implicit val _: ContextShift[IO] = IO.contextShift(testEc)
+        load[IO]
     }
 
     ioRes.unsafeRunSync() shouldBe Config(urlBotapi = "https://URL_BOT/botTOKEN_VALUE", urlCurrency = "URL_CURRENCY")
